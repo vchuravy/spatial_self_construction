21f8e71c5aae6b258d7c7e33c566bbe54e332c58
Valentin Churavy <v.churavy@gmail.com>
2014-03-04 01:12:16 -0500

    refactors potential into a two step calculation

--- potential.jl
+++ potential.jl
@@ -5,15 +5,51 @@
 
 using NumericExtensions
 
+immutable Number4 {T <: FloatingPoint}
+    s0 :: T
+    s1 :: T
+    s2 :: T
+    s3 :: T
+end
+
+function area{T <: FloatingPoint}(direction :: Array{T, 2}, LONG :: Real)
+    D1, D2 = size(direction)
+    SHORT = one(T)
+    dout = Array(Number4, D1, D2)
+    PI_4 = pi/4
+    PI = pi
+
+    for i in 1:length(direction)
+            dir = direction[i];
+
+            area1 = LONG*SHORT/2 * (   PI_4/2-dir - atan((SHORT-LONG)*sin(2*(  PI_4/2-dir)) / (SHORT+LONG + (SHORT-LONG)*cos(2*(  PI_4/2-dir)))));
+            area2 = LONG*SHORT/2 * ( 3*PI_4/2-dir - atan((SHORT-LONG)*sin(2*(3*PI_4/2-dir)) / (SHORT+LONG + (SHORT-LONG)*cos(2*(3*PI_4/2-dir)))));
+            area3 = LONG*SHORT/2 * ( 5*PI_4/2-dir - atan((SHORT-LONG)*sin(2*(5*PI_4/2-dir)) / (SHORT+LONG + (SHORT-LONG)*cos(2*(5*PI_4/2-dir)))));
+            area4 = LONG*SHORT/2 * ( 7*PI_4/2-dir - atan((SHORT-LONG)*sin(2*(7*PI_4/2-dir)) / (SHORT+LONG + (SHORT-LONG)*cos(2*(7*PI_4/2-dir)))));
+            area5 = LONG*SHORT/2 * ( 9*PI_4/2-dir - atan((SHORT-LONG)*sin(2*(9*PI_4/2-dir)) / (SHORT+LONG + (SHORT-LONG)*cos(2*(9*PI_4/2-dir)))));
+
+            lps = SHORT * LONG * PI;
+
+            s0 = (area2-area1)/lps;
+            s1 = (area3-area2)/lps;
+            s2 = (area4-area3)/lps;
+            s3 = (area5-area4)/lps;
+
+            dout[i] = Number4(s0, s1, s2, s3);
+    end
+    return dout
+end
+
+
 function potential(Afield :: Matrix, Bfield :: Matrix, direction :: Matrix, repulsion :: Real, long :: Real)
     d1, d2 = size(Afield)
 
     # is the anisotropic molecule
 
-    Apotential = zeros(Float64, d1, d2)
-    Bpotential = zeros(Float64, d1, d2)
+    Apot = zeros(Float64, d1, d2)
+    Bpot = zeros(Float64, d1, d2)
 
-    short = 1.0
+    Area = area(direction, long)
 
     selfRepulsion = 1*repulsion # making this lower than repulsion allows for neighbors to have relative potential, so increases the chance that the hydrophobe will flow.
 
@@ -25,49 +61,48 @@ function potential(Afield :: Matrix, Bfield :: Matrix, direction :: Matrix, repu
             north = i == d1 ? 1  : i+1
             south = i == 1  ? d1 : i-1
 
-            d  = direction[i,j]
+            a_ij = Area[i, j];
 
-            area1 = area(1.0, long, short, d)
-            area2 = area(3.0, long, short, d)
-            area3 = area(5.0, long, short, d)
-            area4 = area(7.0, long, short, d)
-            area5 = area(9.0, long, short, d)
+            r_ij = selfRepulsion * Afield[i,j];
 
-            lps = long*short*pi
-            a1 = (area2-area1)/lps
-            a2 = (area3-area2)/lps
-            a3 = (area4-area3)/lps
-            a4 = (area5-area4)/lps
+            a_ne = Area[north, east].s0;
+            a_nj = Area[north, j   ].s1;
+            a_nw = Area[north, west].s2;
+            a_iw = Area[i    , west].s3;
+            a_sw = Area[south, west].s0;
+            a_sj = Area[south, east].s1;
+            a_se = Area[south, j   ].s2;
+            a_ie = Area[i    , east].s3;
 
-            cRepulsion = repulsion * Bfield[i,j]
+            r_ne = repulsion * Afield[north, east];
+            r_nj = repulsion * Afield[north, j   ];
+            r_nw = repulsion * Afield[north, west];
+            r_iw = repulsion * Afield[i    , west];
+            r_sw = repulsion * Afield[south, west];
+            r_sj = repulsion * Afield[south, east];
+            r_se = repulsion * Afield[south, j   ];
+            r_ie = repulsion * Afield[i    , east];
 
-            Apotential[i,j] += cRepulsion
-            Apotential[north,east] += cRepulsion*a1
-            Apotential[north,j]    += cRepulsion*a2
-            Apotential[north,west] += cRepulsion*a3
-            Apotential[i,west]     += cRepulsion*a4
+            Bpot[i,j] =    fma(r_ne, a_ne,
+                           fma(r_nj, a_nj,
+                           fma(r_nw, a_nw,
+                           fma(r_iw, a_iw,
+                           fma(r_sw, a_sw,
+                           fma(r_sj, a_sj,
+                           fma(r_se, a_se,
+                           fma(r_ie, a_ie, r_ij))))))));
 
-            Apotential[south,west] += cRepulsion*a1
-            Apotential[south,j]    += cRepulsion*a2
-            Apotential[south,east] += cRepulsion*a3
-            Apotential[i,east]     += cRepulsion*a4
-
-            Bpotential[i,j] += repulsion * ( Afield[i,j]
-                                                + Afield[north,east]  * a1
-                                                + Afield[north,j   ]  * a2
-                                                + Afield[north,west]  * a3
-                                                + Afield[i,west    ]  * a4
-                                                + Afield[south,west]  * a1
-                                                + Afield[south,j   ]  * a2
-                                                + Afield[south,east]  * a3
-                                                + Afield[i,east    ]  * a4
-                                            )
+            Apot[i,j] = repulsion *     fma(Bfield[north,east], a_ij.s0,
+                                        fma(Bfield[north,j   ], a_ij.s1,
+                                        fma(Bfield[north,west], a_ij.s2,
+                                        fma(Bfield[i,west    ], a_ij.s3,
+                                        fma(Bfield[south,west], a_ij.s0,
+                                        fma(Bfield[south,j   ], a_ij.s1,
+                                        fma(Bfield[south,east], a_ij.s2,
+                                        fma(Bfield[i,east    ], a_ij.s3, Bfield[i,j]))))))));
         end
     end
-    return (Apotential,Bpotential)
+    return (Apot,Bpot)
 end
 
-function area(x :: Real, long :: Real, short :: Real, d :: Real)
-    p8 = x * pi/8
-    return long*short/2 * ( p8 - d - atan((short-long)*sin(2*(p8 - d)) / (short+long + (short-long)*cos(2*(p8 - d)))))
-end
+fma(a, b, c) = a * b + c
