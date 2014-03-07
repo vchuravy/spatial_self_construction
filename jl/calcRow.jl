6420dee42b35f7efe48b5fc49d57ca79d45e8d54
Valentin Churavy <v.churavy@gmail.com>
2014-03-07 14:16:23 -0500

    adds julia version of calcRow

---
+++ jl/calcRow.jl
@@ -0,0 +1,13 @@
+function calcRowJl!(
+    M :: Matrix, A :: Matrix, F :: Matrix, W :: Matrix,
+    Out :: Matrix,
+    m :: Real, _a :: Real, f :: Real, w :: Real)
+
+    d1, d2 = size(M)
+
+    for j in 1:d2
+      for i in 1:d1
+        Out[i,j] = pow(M[i, j], m) * pow(A[i, j], _a) * pow(F[i, j], f) * pow(W[i, j], w)
+      end
+    end
+end
