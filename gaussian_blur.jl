aab74e2db6854a0f001beab94f040a08f1cc864b
Valentin Churavy <v.churavy@gmail.com>
2014-03-17 14:26:39 -0400

    proper credit for gaussian_blur, also adds missing code piece

--- gaussian_blur.jl
+++ gaussian_blur.jl
@@ -2,6 +2,12 @@
 # Code taken from https://github.com/timholy/Images.jl
 ###
 
+# The MIT License (MIT)
+# Copyright (c) 2012 Timothy E. Holy
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 
 using Cartesian
 
@@ -139,3 +145,20 @@ function _imfilter_gaussian!{T<:FloatingPoint}(A::Array{T}, sigma::Vector; emit_
     A
 end
 
+function offset(c::Vector{Int}, strds::Vector{Int})
+    o = 0
+    for i = 1:length(c)
+        o += (c[i]-1)*strds[i]
+    end
+    o
+end
+
+function copytail!(dest, A, coloffset, strd, len)
+    for j = 1:3
+        for i = 1:size(dest, 2)
+            tmp = A[i + coloffset + (len-j)*strd]
+            dest[j,i] = tmp
+        end
+    end
+    dest
+end
