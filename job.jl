c3aef6dc86c84d849e3a43eb7aa62d933d23028a
Valentin Churavy <v.churavy@gmail.com>
2014-03-19 15:51:49 -0400

    time main

--- job.jl
+++ job.jl
@@ -1,7 +1,7 @@
 include("main.jl")
 
 function runProcess(config, dist, out, loadTime)
-    r = main(config, dist, true, loadTime = loadTime, resultFolder=out)
+    r = @time main(config, dist, true, loadTime = loadTime, resultFolder=out)
     if length(dist) == 1
         time, value = first(dist)
         name = first(value)
