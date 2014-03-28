d13ea496aae8d9e4a4f7a48138a9bcd12a6f19be
Valentin Churavy <v.churavy@gmail.com>
2014-03-28 14:21:09 -0400

    catch errors in computation

--- job.jl
+++ job.jl
@@ -2,8 +2,11 @@ include("main.jl")
 
 function runProcess(config, val, out, loadTime)
     f, dist = val
-
-    r = @time main(config, dist, true, loadTime = loadTime, resultFolder=out)
     params = first(values(dist))
+    r = try
+        main(config, dist, true, loadTime = loadTime, resultFolder=out)
+    catch
+        (NaN, NaN, NaN, false, NaN, NaN, NaN, NaN, NaN, NaN, NaN, "")
+    end
     return (f, params, r)
 end
